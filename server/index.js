985e686b3f77d23ce2ebe7f82522c6022b14952f
Vamsi-Shetty <bvamsi489@gmail.com>
2024-06-04 20:42:58 +0530

    feat:error handling middleware added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/api/v1/', (req, res) => {
+    res.send("Welcome to Home Page")
+});
+
+app.post('/api/v1/signup', (req, res) => {
+    const [userName, password] = req.body;
+    
+})
+
+app.use((err, req, res, next) => {
+    res.json({
+        message: "Oops, something unexpected happened!"
+    })
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 })
